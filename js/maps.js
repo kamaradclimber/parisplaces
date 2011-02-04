1830638574311c8b8069998ab0d442546fe4397f
Grégoire Seux <kamaradclimber@gmail.com>
2011-02-04 11:22:54 +0100

    jai commencé à ajouter du code pour gérer la carte (mais peut etre que lucas avait deja entamé cette partie)

---
+++ js/maps.js
@@ -0,0 +1,50 @@
+var geocoder;
+var map;
+function initialize() {
+	geocoder = new google.maps.Geocoder();
+	var myLatlng = new google.maps.LatLng(48.8574, 2.3478);
+	var myOptions = {
+zoom: 13,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	map = new google.maps.Map(document.getElementById("map"), myOptions);	
+	codeAddress("76 rue Notre Dame des Champs, Paris");
+	var address;
+	var addresses = document.all("results_zone");
+	for(i=0; i<document.getElementsByName("address").length; i++) {
+		address = document.getElementsByName("address")[i].getAttribute("value");
+		codeAddress(address);
+	}
+}
+
+function codeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var marker = new google.maps.Marker({
+            map: map, 
+            position: results[0].geometry.location
+        });
+	marker.address = address;
+	google.maps.event.addListener(marker, 'mouseover', function() {
+		highlight(marker);
+  	});
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+
+function highlight(marker) {
+
+    		alert(marker.address);
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
