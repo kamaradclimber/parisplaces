6cad41af650309ed8c71d53f153f6e8207d9ab91
Florent Vermeulen <florent.vermeulen@student.ecp.fr>
2011-02-16 16:20:24 +0100

    mise en place server

--- server/play-1.1-v2/myApp/app/controllers/Liste_equipements_de_proximite_2011.java
+++ server/play-1.1-v2/myApp/app/controllers/Liste_equipements_de_proximite_2011.java
@@ -1,8 +1,15 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Properties;
 
 import models.Place;
 
@@ -10,8 +17,8 @@ public class Liste_equipements_de_proximite_2011 extends CsvConnector {
 
 	public Liste_equipements_de_proximite_2011()
 			throws SQLException {
-		super("data/Liste_equipements_de_proximite_2011.csv");
-		super.hierarchy = new Categories("Ecole Maternelle");
+		super("../data/Liste_equipements_de_proximite_2011.csv");
+		super.hierarchy = new Categories("Ecole Maternelle");				
 		}
 
 	@Override
@@ -30,6 +37,8 @@ public class Liste_equipements_de_proximite_2011 extends CsvConnector {
 		ArrayList<Place> places = new ArrayList<Place>();
 		Place currentPlace;
 		while (rs.next()){
+			//System.out.println("place:"+rs.getString(1)+"/"+rs.getString(2)+"/"+rs.getString(3)+"/"+rs.getString(4)+"/"+rs.getString(5)+"/"+rs.getString(6)+"/"+rs.getString(7));
+			if (rs.getString(1)==null) continue;
 			String dis = rs.getString(1).substring(11, rs.getString(1).length()-2 );
 			int district;
 			if (dis.length()==0) 
@@ -38,6 +47,7 @@ public class Liste_equipements_de_proximite_2011 extends CsvConnector {
 				district = Integer.parseInt(dis);
 			currentPlace = new Place(rs.getString(3), district, rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(6), rs.getString(7));
 			places.add(currentPlace);
+			System.out.println(currentPlace);
 		}
 		return places;
 	}
