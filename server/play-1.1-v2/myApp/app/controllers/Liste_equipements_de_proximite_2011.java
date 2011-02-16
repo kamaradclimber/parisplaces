d20139c7b16d480c0d89d90c8be380e11911a36c
Florent Vermeulen <florent.vermeulen@student.ecp.fr>
2011-02-16 16:32:36 +0100

    ajout données

--- server/play-1.1-v2/myApp/app/controllers/Liste_equipements_de_proximite_2011.java
+++ server/play-1.1-v2/myApp/app/controllers/Liste_equipements_de_proximite_2011.java
@@ -17,7 +17,7 @@ public class Liste_equipements_de_proximite_2011 extends CsvConnector {
 
 	public Liste_equipements_de_proximite_2011()
 			throws SQLException {
-		super("../data/Liste_equipements_de_proximite_2011.csv");
+		super("data/Liste_equipements_de_proximite_2011.csv");
 		super.hierarchy = new Categories("Ecole Maternelle");				
 		}
 
